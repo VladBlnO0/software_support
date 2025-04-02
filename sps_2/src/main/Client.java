da11a731412106b48e3044567c492a8db9457970
VlBl <vladpuku@gmail.com>
2025-04-02 20:04:12 +0300

    Finalizing of project to start

--- sps_2/src/main/Client.java
+++ sps_2/src/main/Client.java
@@ -1,9 +1,13 @@
 package main;
 
 public class Client {
+    private final String Company_Name, Address, Phone_Number;
     Contract contr;
 
     Client(String Company_Name, String Address, String Phone_Number, Contract contr) {
+        this.Company_Name = Company_Name;
+        this.Address = Address;
+        this.Phone_Number = Phone_Number;
         this.contr = contr;
     }
 }
